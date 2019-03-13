1ab20428dcd42ab67e93824903378e9da3dd9bfd
HorrorRice <rifqi@noreply.github.com>
2019-03-13 22:49:00 +0700

    update

--- mooc-OOProgrammingWithJava-PART2/week7/week7-03.Calculator/src/Calculator.java
+++ mooc-OOProgrammingWithJava-PART2/week7/week7-03.Calculator/src/Calculator.java
@@ -11,8 +11,6 @@ public class Calculator {
 
     private Reader input;
     private int calculatorLog;
-    private int value1;
-    private int value2;
 
     public Calculator() {
         input = new Reader();
@@ -20,92 +18,63 @@ public class Calculator {
     }
 
     public void start() {
-        System.out.println("|----- Command Line Calculator -----|");
-        System.out.println("type: 'help' to see all available commands.");
+
         while (true) {
             System.out.print("command: ");
             String command = input.readString();
             if (command.equals("end")) {
                 break;
             }
-            if (command.equals("sum") || command.equals("+")) {
+
+            if (command.equals("sum")) {
                 sum();
-            } else if (command.equals("multiply") || command.equals("*")) {
-                multiply();
-            } else if (command.equals("substract") || command.equals("-")) {
-                substract();
-            } else if (command.equals("divide") || command.equals("/")) {
-                divide();
-            } else if (command.equals("mod") || command.equals("%")) {
-                modulus();
-            } else if (command.equals("help") || command.equals("?")) {
-                help();
-            } else if (command.equals("log")) {
-                statistics();
+            } else if (command.equals("product")) {
+                product();
+            } else if (command.equals("difference")) {
+                difference();
             }
         }
         statistics();
 
     }
 
-    public void inputValue() {
-        System.out.print("value1: ");
-        this.value1 = this.input.readInteger();
-        System.out.print("value2: ");
-        this.value2 = this.input.readInteger();
-    }
+    private void sum() {
+        calculatorLog++;
 
-    public void sum() {
-        inputValue();
+        System.out.print("Value1: ");
+        int value1 = this.input.readInteger();
+        System.out.print("Value2: ");
+        int value2 = this.input.readInteger();
 
-        int total = this.value1 + this.value2;
+        int total = value1 + value2;
         System.out.println("sum of the values " + total);
+    }
+
+    private void product() {
         calculatorLog++;
+
+        System.out.print("value1: ");
+        int value1 = input.readInteger();
+        System.out.print("value2: ");
+        int value2 = input.readInteger();
+
+        int total = value1 * value2;
+        System.out.println("product of the values: " + total);
     }
 
-    public void multiply() {
-        inputValue();
-
-        int total = this.value1 * this.value2;
-        System.out.println("multiplication of the values: " + total);
+    private void difference() {
         calculatorLog++;
+
+        System.out.print("value1: ");
+        int value1 = input.readInteger();
+        System.out.print("value2: ");
+        int value2 = input.readInteger();
+
+        int total = value1 - value2;
+        System.out.println("difference of the values: " + total);
     }
 
-    public void substract() {
-        inputValue();
-
-        int total = this.value1 - this.value2;
-        System.out.println("substraction of the values: " + total);
-        calculatorLog++;
-    }
-
-    public void divide() {
-        inputValue();
-
-        int total = this.value1 / this.value2;
-        System.out.println("division of the values: " + total);
-        calculatorLog++;
-    }
-
-    public void modulus() {
-        inputValue();
-
-        int total = this.value1 % this.value2;
-        System.out.println("modulus of the values: " + total);
-        calculatorLog++;
-    }
-
-    public void help() {
-        System.out.println("List Command :\n'+' 'sum' - addition\n"
-                + "'*' 'multiply' - multiplication\n"
-                + "'-' 'substract' - substraction\n"
-                + "'/' 'divide' - division\n"
-                + "'?' 'help' - show list of commands\n"
-                + "'log' - show log\n"
-                + "'end' - quit calculator");
-    }
-
-    public void statistics() {
+    private void statistics() {
         System.out.println("Calculations done " + calculatorLog);
     }
 }
